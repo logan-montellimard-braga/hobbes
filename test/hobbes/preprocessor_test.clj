1f6933fbe021280c98da285b1ff18257cc51be46
Logan Braga <braga.logan@gmail.com>
2015-01-14 17:58:59 +0100

    Coding standard : from @#' to #' to access private func in testing

--- test/hobbes/preprocessor_test.clj
+++ test/hobbes/preprocessor_test.clj
@@ -5,32 +5,32 @@
 (deftest removing-comments
   (testing "Cleaning comments from monoline input"
     (is (= ""
-           (@#'hobbes.preprocessor/remove-comments "!! Commented !"))))
+           (#'hobbes.preprocessor/remove-comments "!! Commented !"))))
   (testing "Cleaning comments in input without comments"
     (is (= "Untouched."
-           (@#'hobbes.preprocessor/remove-comments "Untouched."))))
+           (#'hobbes.preprocessor/remove-comments "Untouched."))))
   (testing "Cleaning comments from multiline input"
     (is (= "Not commented.\n here neither"
-           (@#'hobbes.preprocessor/remove-comments "Not commented.\n!! commented\n here neither")))))
+           (#'hobbes.preprocessor/remove-comments "Not commented.\n!! commented\n here neither")))))
 
 (deftest triming-lines
   (testing "Trimming whitespaces"
     (is (= "Perfectly trimmed.\nHere too."
-           (@#'hobbes.preprocessor/trim-each-line "   Perfectly trimmed.  \n Here too.  "))))
+           (#'hobbes.preprocessor/trim-each-line "   Perfectly trimmed.  \n Here too.  "))))
   (testing "Trimming tabs and feeds"
     (is (= "Perfectly trimmed.\nHere too."
-           (@#'hobbes.preprocessor/trim-each-line "\tPerfectly trimmed.\n\tHere too.\f")))))
+           (#'hobbes.preprocessor/trim-each-line "\tPerfectly trimmed.\n\tHere too.\f")))))
 
 (deftest adding-padding
   (testing "Input already has two padding newlines."
     (is (= "Already there\n\n"
-           (@#'hobbes.preprocessor/add-padding-newlines "Already there\n\n"))))
+           (#'hobbes.preprocessor/add-padding-newlines "Already there\n\n"))))
   (testing "Input has one padding newline."
     (is (= "One is here\n\n"
-           (@#'hobbes.preprocessor/add-padding-newlines "One is here\n"))))
+           (#'hobbes.preprocessor/add-padding-newlines "One is here\n"))))
   (testing "Input already has no padding newlines."
     (is (= "Not yet\n\n"
-           (@#'hobbes.preprocessor/add-padding-newlines "Not yet")))))
+           (#'hobbes.preprocessor/add-padding-newlines "Not yet")))))
 
 
 (deftest putting-it-all-together
