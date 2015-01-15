64ee814e05d2ab7c415e955cb9bd1094059b1b57
Logan Braga <braga.logan@gmail.com>
2015-01-15 18:01:46 +0100

    Testing abbreviation expansion in preprocessor

--- test/hobbes/preprocessor_test.clj
+++ test/hobbes/preprocessor_test.clj
@@ -13,13 +13,31 @@
     (is (= ["Untouched."]
            (#'hobbes.preprocessor/remove-comments "Untouched.")))))
 
-(deftest adding-padding
+(deftest add-padding-newlines
   (testing "Input has no padding newlines."
     (is (= "Not yet\n\n"
            (#'hobbes.preprocessor/add-padding-newlines "Not yet")))))
 
+(deftest expand-abbrevs
+  (testing "Input has no abbreviation pattern and no abbr map."
+    (is (= "Nothing changed."
+           (#'hobbes.preprocessor/expand-abbrevs {} "Nothing changed."))))
+  (testing "Input has no abbreviation pattern but an abbr map."
+    (is (= "Nothing changed."
+           (#'hobbes.preprocessor/expand-abbrevs {:Nothing :Something}
+                                                 "Nothing changed."))))
+  (testing "Input has abbreviation pattern but no abbr map."
+    (is (= "~Nothing changed."
+           (#'hobbes.preprocessor/expand-abbrevs {}
+                                                 "~Nothing changed."))))
+  (testing "Input has abbreviation pattern and abbr map."
+    (is (= "Something changed."
+           (#'hobbes.preprocessor/expand-abbrevs {:Nothing :Something}
+                                                 "~Nothing changed.")))))
+
 
 (deftest putting-it-all-together
   (testing "Preprocessing multiline input."
-    (is (= "Trimmed,\nno comments,\npadding.\n\n"
-           (preprocess "  \tTrimmed,\n!!comment\nno comments,\npadding.")))))
+    (is (= "Trimmed,\nno comments,\nnew-padding.\n\n"
+           (preprocess "  \tTrimmed,\n!!comment\nno comments,\n~padding."
+                       {:padding :new-padding})))))
