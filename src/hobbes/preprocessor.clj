cd75a2e9a7027ac1de842c6f2882cecafbc5429e
Logan Braga <braga.logan@gmail.com>
2015-01-15 14:15:50 +0100

    Moved map general logic out of preprocessor to utils.clj

--- src/hobbes/preprocessor.clj
+++ src/hobbes/preprocessor.clj
@@ -1,6 +1,7 @@
 (ns hobbes.preprocessor
   "Engine used to preprocess input string for optimal parsing."
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as s]
+            [hobbes.utils :refer :all]))
 
 ; Treat input formatting
 (def ^:private comment-pattern #"^\s*!!.*")
@@ -22,26 +23,6 @@
 
 
 ; Expand abbreviations
-(defn map-replace
-  "Takes a map of {k v} and a string and replaces every occurence of k with v
-  in the string, for each k, v in the map. k and v may be keywords or strings.
-  Returns the treated string."
-  ([string] string)
-  ([m string]
-   (reduce
-     (fn [acc [k v]] (s/replace acc (name k) (name v)))
-     string m)))
-
-(defn add-prefix-to-map-keys
-  "Takes a map and a prefix string, and adds the prefix to each key of the map,
-  converting them to strings.
-  Returns the treated map."
-  ([m] m)
-  ([m prefix]
-  (into (empty m)
-        (for [[k v] m]
-          [(str prefix (name k)) v]))))
-
 (defn- expand-abbrevs
   "Take a string input and a map of abbreviations, and expands all of them.
   Returns the treated string input."
