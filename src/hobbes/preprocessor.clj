bda47c28f640d27583da7201ce6c3350e92069fd
Logan Braga <braga.logan@gmail.com>
2015-01-15 11:22:18 +0100

    Added abbreviations expansion logic in preprocessor

--- src/hobbes/preprocessor.clj
+++ src/hobbes/preprocessor.clj
@@ -2,11 +2,11 @@
   "Engine used to preprocess input string for optimal parsing."
   (:require [clojure.string :as s]))
 
+; Treat input formatting
 (def ^:private comment-pattern #"^\s*!!.*")
 
 (defn- remove-comments
-  "Remove comment lines from input, be it a string or
-  a coll of strings.
+  "Remove comment lines from input, be it a string or a coll of strings.
   Returns a coll."
   [input]
   (let [coll (if (coll? input) input (vector input))]
@@ -20,6 +20,38 @@
   [input]
   (str input "\n\n"))
 
+
+; Expand abbreviations
+(defn map-replace
+  "Takes a map of {k v} and a string and replaces every occurence of k with v
+  in the string, for each k, v in the map. k and v may be keywords or strings.
+  Returns the treated string."
+  ([string] string)
+  ([m string]
+   (reduce
+     (fn [acc [k v]] (s/replace acc (name k) (name v)))
+     string m)))
+
+(defn add-prefix-to-map-keys
+  "Takes a map and a prefix string, and adds the prefix to each key of the map,
+  converting them to strings.
+  Returns the treated map."
+  ([m] m)
+  ([m prefix]
+  (into (empty m)
+        (for [[k v] m]
+          [(str prefix (name k)) v]))))
+
+(defn- expand-abbrevs
+  "Take a string input and a map of abbreviations, and expands all of them.
+  Returns the treated string input."
+  [string abbrs]
+  (-> abbrs
+       (add-prefix-to-map-keys "~")
+       (map-replace string)))
+
+
+; Public API
 (defn preprocess
   "Preprocess input, making it suitable to parse.
   Returns the treated input as string."
