e2165fecf8cf4f8c20ea91ac15f3735e35e28d75
Logan Braga <braga.logan@gmail.com>
2015-01-18 21:53:31 +0100

    Adding namespaces loading and docstring to hobbes.core

--- src/hobbes/core.clj
+++ src/hobbes/core.clj
@@ -1,4 +1,9 @@
 (ns hobbes.core
+  "Hobbes main namespace.
+  Manages the whole program by stitching other namespaces together."
+  (:require [hobbes.utils :refer :all]
+            [hobbes.preprocessor :as pre]
+            [hobbes.engine :as engine])
   (:gen-class))
 
 (defn -main
