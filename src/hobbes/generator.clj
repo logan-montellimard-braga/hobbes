2f35f531ba03fa10aaae33ac84b61210ad913edc
Logan Braga <braga.logan@gmail.com>
2015-01-25 15:29:28 +0100

    HTML generator namespace created

---
+++ src/hobbes/generator.clj
@@ -0,0 +1,5 @@
+(ns hobbes.generator
+  "Generator namespace, used to transform valid HTML5 AST into complete web
+  pages."
+  (:require [net.cgrand.enlive-html :as html]
+            [hobbes.utils :refer :all]))
