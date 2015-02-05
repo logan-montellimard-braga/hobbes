028cb920ecc1ea817c7d082415f4569bdeadeeb6
Logan Braga <braga.logan@gmail.com>
2015-02-05 20:41:13 +0100

    Generator test: get-tmpl-name test

--- test/hobbes/generator_test.clj
+++ test/hobbes/generator_test.clj
@@ -1,3 +1,11 @@
 (ns hobbes.generator-test
   (:require [clojure.test :refer :all]
             [hobbes.generator :refer :all]))
+
+(deftest get-tmpl-name-test
+  (testing "Incorrect name path"
+    (is (= nil
+           (#'hobbes.generator/get-tmpl-name "kdjskdz"))))
+  (testing "Correct name path (contained in default resource)"
+    (is (= (clojure.java.io/resource "default/template/layout.html")
+           (#'hobbes.generator/get-tmpl-name "layout")))))
