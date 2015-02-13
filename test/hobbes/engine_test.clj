cb882e272edffd3988d6e897358deae9bca644fc
Logan Braga <braga.logan@gmail.com>
2015-02-13 22:21:26 +0100

    Fixed em, strong and mark tags to reflect HTML5 recommandations

--- test/hobbes/engine_test.clj
+++ test/hobbes/engine_test.clj
@@ -12,10 +12,12 @@
     (binding [*out* nil]
       (is (thrown? java.lang.Exception (#'hobbes.engine/parse-blocks "a")))))
   (testing "Correct, simple input"
-    (is (= '({:tag :HEADER, :content ({:tag :H1, :content nil} "foo" {:tag :EOL, :content nil})})
+    (is (= '({:tag :HEADER, :content
+              ({:tag :H1, :content nil} "foo" {:tag :EOL, :content nil})})
            (#'hobbes.engine/parse-blocks "_t foo\n"))))
   (testing "Flexibility of special tokens"
-    (is (= '({:tag :HEADER, :content ({:tag :H2, :content nil} "foo" {:tag :EOL, :content nil})})
+    (is (= '({:tag :HEADER, :content
+              ({:tag :H2, :content nil} "foo" {:tag :EOL, :content nil})})
            (#'hobbes.engine/parse-blocks "_stit foo\n")))))
 
 (deftest parse-spans-test
@@ -26,18 +28,20 @@
     (is (= "foo*bar"
            (#'hobbes.engine/parse-spans "foo*bar"))))
   (testing "String with one span to detect"
-    (is (= '("foo " {:tag :b, :content "bold", :attrs nil} " not bold")
+    (is (= '("foo " {:tag :strong, :content "bold", :attrs nil} " not bold")
            (#'hobbes.engine/parse-spans "foo *bold* not bold"))))
   (testing "String with several spans to detect"
-    (is (= '("foo " {:tag :b, :content "bold", :attrs nil}
-                  (" " {:tag :i, :content "italic", :attrs nil} " normal"))
+    (is (= '("foo " {:tag :strong, :content "bold", :attrs nil}
+                    (" " {:tag :em, :content "italic", :attrs nil} " normal"))
            (#'hobbes.engine/parse-spans "foo *bold* /italic/ normal"))))
   (testing "String with nested spans"
-    (is (= '("" {:tag :b, :content ("bold" {:tag :u, :content "underline", :attrs nil} ""), :attrs nil} "")
+    (is (= '("" {:tag :strong
+                 :content ("bold" {:tag :u, :content "underline", :attrs nil}
+                                  ""), :attrs nil} "")
            (#'hobbes.engine/parse-spans "*bold_underline_*"))))
   (testing "Special case: link span"
-    (is (= '("here a " {:tag :a, :content "Link", :attrs {:href "link.html"
-                                                         :class "internal"}} "")
+    (is (= '("here a " {:tag :a, :content "Link",
+                        :attrs {:href "link.html" :class "internal"}} "")
            (#'hobbes.engine/parse-spans "here a ->link")))))
 
 (deftest split-at-blocks-test
@@ -54,5 +58,7 @@
            (parse ""))))
   (testing "Real input"
     (is (= '({:tag :h1, :content "My title", :attrs {:id "my_title"}}
-             {:tag :p, :content ("my paragraph in " {:tag :b, :content "bold", :attrs nil} "")})
+             {:tag :p, :content ("my paragraph in "
+                                 {:tag :strong, :content "bold", :attrs nil}
+                                 "")})
            (parse "_Ti My title\nmy paragraph in *bold*\n\n")))))
