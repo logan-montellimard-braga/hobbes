db71fa3d9bf63a13fa3e6246a0ce59fc90f904bc
Logan Braga <braga.logan@gmail.com>
2015-02-17 16:41:54 +0100

    Use raynes' fs lib to iterate over files

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "GNU/GPL v3"
             :url "http://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 ; [org.clojure/tools.cli "0.3.1"]
                  ; [seesaw "1.4.5" :exclusions [org.clojure/clojure]]
+                 [me.raynes/fs "1.4.6"]
                  [enlive "1.1.5"]
                  [instaparse "1.3.5"]]
   :bin {:name "hobbes"}
